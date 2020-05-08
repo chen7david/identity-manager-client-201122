e4d3a9f40f30863badf0c50946774e9b31316b2e
David Chen <chen7david@me.com>
2020-05-08 10:37:57 +0800

    update

--- src/views/QRAuth.vue
+++ src/views/QRAuth.vue
@@ -6,24 +6,40 @@
                     <v-list-item two-line>
                         <v-list-item-content>
                             <div class="overline">device authorization login</div>
-                            <v-list-item-subtitle>Authorize device for QR-login:{{code}}</v-list-item-subtitle>
                         </v-list-item-content>
                     </v-list-item>  
                 </template>
 
                 <template v-slot:image>      
                     <v-col>
-                        :{{QRAuthInfo}}
-                            :{{validQR}}
-                            :{{device}}
                         <v-avatar color="blue" size="180">
+                            <div v-if="validQR">
+                                <v-icon size="100px" v-if="QRAuthInfo.isDesktop">mdi-desktop-mac</v-icon>
+                                <v-icon size="100px" v-if="QRAuthInfo.isTablet">mdi-tablet-ipad</v-icon>
+                                <v-icon size="100px" v-if="QRAuthInfo.isiPhone">mdi-cellphone-iphone</v-icon>
+                                <v-icon size="100px" v-if="QRAuthInfo.isBot">mdi-memory</v-icon>
+                                <v-icon size="100px" v-if="QRAuthInfo.isTV">mdi-monitor</v-icon>
+                            </div>
+                            <div v-else>
+                                <v-icon size="100px">mdi-information</v-icon>
+                            </div>
+
                         </v-avatar>
+                        <v-chip v-if="validQR" class="overline mt-5">
+                            {{QRAuthInfo.os}}
+                            {{QRAuthInfo.platform}}
+                            {{QRAuthInfo.browser}}
+                            {{QRAuthInfo.version}}
+                        </v-chip>
+                        <v-chip v-else class="overline mt-5">
+                            please refresh the QR code!
+                        </v-chip>
                     </v-col>
                 </template>
 
                 <template v-slot:actions>      
                     <v-col>
-                        <v-btn :loading="isLoading" large class="mt-0" elevation="0" block @click="qrlogin(code)">login</v-btn>
+                        <v-btn :disabled="!validQR" :loading="isLoading" large class="mt-0" elevation="0" block @click="qrlogin(code)">login</v-btn>
                         <v-divider class="my-5"></v-divider>
                         <v-btn large elevation="0" block router to="/register">cancel</v-btn>
                     </v-col>
@@ -37,13 +53,13 @@
 import { mapGetters, mapActions } from 'vuex'
 import FlexCenter from './../components/layout/FlexCenter'
 import FormCard from './../components/layout/FormCard'
+import UA from 'useragent-parser-js'
 export default {
     name:'Login',
     data: () => ({
         code: null,
-        QRAuthInfo: null,
-        validQR: true,
-        device: null
+        QRAuthInfo: {},
+        validQR: true
     }),
     components: {
         FlexCenter,
@@ -60,7 +76,8 @@ export default {
     sockets:{
         qrauthinfo(data){
             this.validQR = data ? true : false
-            this.QRAuthInfo = data ? data : {}
+            // if(this.validQR) 
+            this.QRAuthInfo = UA.parse(data && data.useragent || '') 
         }
     },
     methods: {
